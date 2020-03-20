cb329b76669d9a9ded7932cac8625341d6e1855b
Sydney Myers <myerssm02@uww.edu>
2020-03-20 02:29:17 -0500

    well if you ever needed the location of the mouse at any time we now how two scripts for that... very exciting

---
+++ AIProject/Assets/Scripts/MouseCalculations.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseCalculations : MonoBehaviour
+{
+    private Camera cam;
+
+    private void Start()
+    {
+        cam = Camera.main;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 mouse = Input.mousePosition;
+
+        //is all this nessessary? i have no idea
+        Vector3 pointLocation = cam.WorldToScreenPoint(transform.localPosition);
+
+        Vector2 offset = new Vector2(mouse.x - pointLocation.x, mouse.y - pointLocation.y);
+
+        float angle = Mathf.Atan2(offset.y, offset.x) * Mathf.Rad2Deg;
+        transform.rotation = Quaternion.Euler(0f, 0f, angle);
+
+
+    }
+}
---
+++ AIProject/Assets/Scripts/crosshair.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class crosshair : MonoBehaviour
+{
+
+
+    private Vector3 target;
+    public GameObject cross;
+
+    private void Start()
+    {
+        Cursor.visible = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        target = transform.GetComponent<Camera>().ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, transform
+            .position.z));
+        cross.transform.position = new Vector2(target.x, target.y);
+    }
+}
