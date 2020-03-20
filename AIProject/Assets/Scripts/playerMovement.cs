5beb0d51be91aabe842fa45b6e1fbb6524402ad6
Sydney Myers <myerssm02@uww.edu>
2020-03-19 19:26:30 -0500

    player movement

---
+++ AIProject/Assets/Scripts/playerMovement.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerMovement : MonoBehaviour
+{
+    public float speed = 5f;
+    public Rigidbody2D rb;
+    bool facingRight = true;
+
+    Vector2 movement;
+    public Animator anim;
+
+    void Update()
+    {
+        //input
+
+        movement.x = Input.GetAxisRaw("Horizontal");
+        movement.y = Input.GetAxisRaw("Vertical");
+
+        anim.SetFloat("H", movement.x);
+        anim.SetFloat("V", movement.y);
+        anim.SetFloat("speed", movement.sqrMagnitude);
+
+
+        if (facingRight == false && movement.x > 0)
+        {
+            //sr.flipX = true;
+
+            Flip();
+        }
+        else if (movement.x < 0 && facingRight == true)
+        {
+            //sr.flipX = false;
+            Flip();
+        }
+
+    }
+    void Flip()
+    {
+        facingRight = !facingRight;
+        Vector3 Scale = transform.localScale;
+        Scale.x *= -1;
+        transform.localScale = Scale;
+    }
+
+    void FixedUpdate()
+    {
+        //movement
+
+        rb.MovePosition(rb.position + (movement * speed * Time.fixedDeltaTime));
+    }
+
+}
