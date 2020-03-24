e3773dff43ed242c5cc08f4d1205ede0badead28
Flare26 <47835828+Flare26@users.noreply.github.com>
2020-03-23 19:19:38 -0500

    Began pathfinding stuff

---
+++ AIProject/Assets/Scripts/Pathfinding.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pathfinding
+{
+    // constructor
+    public Pathfinding( int x, int y)
+    {
+
+
+    }
+}
