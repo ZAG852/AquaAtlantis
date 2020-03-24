deaad657cfb68e53530e673e5d58e73c439abfd0
Flare26 <47835828+Flare26@users.noreply.github.com>
2020-03-23 20:09:38 -0500

    2

--- AIProject/Assets/Scripts/WorldGrid.cs
+++ AIProject/Assets/Scripts/WorldGrid.cs
@@ -2,17 +2,30 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class MapGrid : MonoBehaviour
+public class WorldGrid : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private int levelWidth;
+    private int levelHeight;
+    private int[,] grid2d; // 2D array
+    public WorldGrid(int width, int height)
     {
+        levelWidth = width;
+        levelHeight = height;
+
+
+        grid2d = new int[width, height]; // rows.len = width cols.len = height
+
         
+
+        for (int i = 0; i < grid2d.GetLength(0); i ++)
+        {
+            for (int j = 0; j < grid2d.GetLength(1); j++)
+            {
+                Debug.Log(i + " " + j);
+            }
+        }
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+
+
 }
