47c7129a861c6ec228d5b4b1724a627c5bf0b29d
Nick <nickthwang@gmail.com>
2020-04-25 13:08:57 -0500

    audio inital push

---
+++ AIProject/Assets/Audio/enemySonicEssence.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+[RequireComponent(typeof(AudioSource))]
+public class enemySonicEssence : MonoBehaviour
+{
+    [SerializeField] enemyOptions enemy;
+
+    AudioSource essenceSource;
+
+    private void Awake()
+    {
+        essenceSource = GetComponent<AudioSource>();
+        essenceSource.playOnAwake = true;
+    }
+
+    void Start()
+    {
+        // set output routing
+        AudioMixer mixer = Resources.Load("MainMixer") as AudioMixer;
+        AudioMixerGroup enemyGroup = mixer.FindMatchingGroups("Master/EnemyEssence")[0];
+
+        essenceSource.outputAudioMixerGroup = enemyGroup;
+
+        // set loop
+        if (!essenceSource.loop)
+            essenceSource.loop = true;
+
+        // set random pitch
+        essenceSource.pitch = Random.Range(.9f, 1.2f);
+
+        // set audio clip
+        essenceSource.clip = AudioManager.audioManager.GetEnemyEssenceClip(enemy);
+
+        // play
+        if (!essenceSource.isPlaying)
+            essenceSource.Play();
+    }
+}
---
+++ AIProject/Assets/Scripts/SceneController.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+// basic scene controller - NH
+public class SceneController : MonoBehaviour
+{
+    public static SceneController sceneController;
+
+    [SerializeField] int currentSceneIndex;
+
+    private void Awake()
+    {
+        if (sceneController == null)
+        {
+            sceneController = this;
+        } else
+        {
+            Destroy(gameObject);
+        }
+
+        // grabs the current scene's index
+        currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        SceneManager.sceneLoaded += updateCurrentSceneIndex;
+    }
+
+    private void OnDisable()
+    {
+        SceneManager.sceneLoaded -= updateCurrentSceneIndex;
+    }
+
+    void updateCurrentSceneIndex(Scene scene, LoadSceneMode mode)
+    {
+        currentSceneIndex = scene.buildIndex;
+    }
+
+    // feed the desired scene index to load         
+    public void loadScene(int sceneIndex)
+    {
+        SceneManager.LoadScene(sceneIndex);
+    }
+
+    // feed the desired scene name (a string -- use QUOTES) to load
+    public void loadScene(string sceneName)
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+
+    // adds 1 to the current scene index and loads it
+    public void loadNextScene()
+    {
+        SceneManager.LoadScene(currentSceneIndex + 1);
+    }
+
+    // reload the current scene
+    public void ReloadScene()
+    {
+        SceneManager.LoadScene(currentSceneIndex);
+    }
+
+    public void delayedNextLoad(float delay)
+    {
+        Invoke("loadNextScene", delay);
+    }
+
+    public void delayedReload(float delay)
+    {
+        Invoke("ReloadScene", delay);
+    }
+}
