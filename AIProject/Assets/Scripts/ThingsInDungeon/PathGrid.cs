3c844796d7147849ccbde9d14e4fb315a2f504ac
Flare26 <47835828+Flare26@users.noreply.github.com>
2020-05-05 19:01:14 -0500

    fixed grid system

--- AIProject/Assets/Scripts/ThingsInDungeon/PathGrid.cs
+++ AIProject/Assets/Scripts/ThingsInDungeon/PathGrid.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -6,75 +7,112 @@ public class PathGrid : MonoBehaviour
 {
 
     // Center of room X coord, Y coord
-    public GameObject player;
+    
     public PathNode node;
-    public double pX = 0;
-    public double pY = 0;
-    public string playerIsAt;
-    static int gridRes = 200; // SIDE LENGTH IS SQRT OF THIS NUM | This is grid resolution. Higher = more node objects
-   
+    public GameObject player;
+    public List<PathNode> current_neighbors;
+    public Vector2Int N;
+    public Vector2Int S;
+    public Vector2Int E;
+    public Vector2Int W;
+    public float Xidx;
+    public float Yidx;
+    static int gridX = 200; // being the rightmost X coordinate
+    public float ws;
 
     PathNode[,] mstrGrid;
-    int rowCount = (int)Mathf.Sqrt(gridRes);
-    int cX = 0;
-    int cY = 0;
-    
     
     //World bounds, node side length, 
     // Start is called before the first frame update
     void Start()
     {
         // each node takes up 5x5 space so div the world size by 10 to create appropriate # of path node entries in the array
-        player = GameObject.FindGameObjectWithTag("Player");
         
-        float ws = MapMaker.mapThingy.getWorldSize(); // world size is length of one side
-        print(ws);
-        
-        
-        double B = ((double)( ws*ws ) / gridRes);
-        mstrGrid = new PathNode[(int) B, (int) B];
+        ws = MapMaker.mapThingy.getWorldSize(); // world size is length of one side
+        float xLength = (ws / gridX);
+        mstrGrid = new PathNode[gridX, gridX];
 
 
-        for (int x = 0; x < rowCount ; x++)
+        for (int x = 0; x < gridX ; x++)
         {
 
-            for (int y = 0; y < rowCount ; y++)
+            for (int y = 0; y < gridX ; y++)
             {
                 // creates reference to the object stored in node
                 // call master grid to return a node and set the coordinate properties.
                 // cX & cY are both based on nodeArea
 
-                mstrGrid[x, y] = Instantiate(node); 
-                //mstrGrid[x, y] = this.gameObject.AddComponent<PathNode>(); // returns game object                
-                mstrGrid[x, y].area = gridRes;               
-                mstrGrid[x, y].X = cX;
-                mstrGrid[x, y].Y = cY;
-                mstrGrid[x, y].IDX = "[" + x + "," + y + "]"; // stores idx string
-                cY += rowCount;
+                mstrGrid[x, y] = new PathNode(new Vector2Int(x,y)); 
+   
             }
-            cX += rowCount;
-            cY = 0;// account for node area and bring row back
         }
-        print(mstrGrid.Length);
     }
-
+    public List<PathNode> findNeighbors(Vector2Int nodelabel)
+    {
+        // Stores a vector2 for each neighbor. Vector 2 corresponds to row/col in mstrGrid[,]. Reverse Astar solve from player to given enemy;
+        List<PathNode> neighbors = new List<PathNode>(4);
+        
+        try
+        {
+            neighbors.Add(mstrGrid[nodelabel.x + 1, nodelabel.y]);
+            E = mstrGrid[nodelabel.x + 1, nodelabel.y].nodePosition;
+        } catch( IndexOutOfRangeException e)
+        {
+            Debug.Log("No more Eastern nodes!");
+            E = new Vector2Int(-1, -1);
+        }
+        try
+        {
+            neighbors.Add(mstrGrid[nodelabel.x - 1, nodelabel.y]);
+            W = mstrGrid[nodelabel.x - 1, nodelabel.y].nodePosition;
+        }
+        catch (IndexOutOfRangeException e)
+        {
+            Debug.Log("No more Western nodes!");
+            W = new Vector2Int(-1, -1);
+        }
+        try
+        {
+            neighbors.Add(mstrGrid[nodelabel.x, nodelabel.y + 1]);
+            N = mstrGrid[nodelabel.x - 1, nodelabel.y].nodePosition;
+        }
+        catch (IndexOutOfRangeException e)
+        {
+            Debug.Log("No more Northern nodes!");
+        }
+        try
+        {
+            neighbors.Add(mstrGrid[nodelabel.x, nodelabel.y-1]);
+            S = mstrGrid[nodelabel.x, nodelabel.y-1].nodePosition;
+        }
+        catch (IndexOutOfRangeException e)
+        {
+            Debug.Log("No more Southern nodes!");
+            S = new Vector2Int(-1, -1);
+        }
+        neighbors.TrimExcess();
+        return neighbors;
+    }
+    
+    Vector2Int Translate(Vector3 position)
+    {
+        // Take in player position in the game space, translate to path grid index in form of a vector2
+        // Numbers between 0 and 1 where n * world size = coordinate
+        float nXposition = position.x / ws;
+        float nYposition = position.y / ws;
+        // Node diameter is accounted for by rounding
+        int xg = Mathf.RoundToInt(nXposition * gridX);
+        int yg = Mathf.RoundToInt(nYposition * gridX);
+        return new Vector2Int(xg, yg);
+    }
     // Update is called once per frame
     void Update()
     {
-
-        for ( int i = 0; i < rowCount; i++)
-        {
-           for ( int j = 0; j < rowCount; j ++ )
-            {
-                if (mstrGrid[i, j].containsPlayer)
-                {
-                    playerIsAt = mstrGrid[i, j].IDX;
-                }
-            }
-        }
-
-        pX = player.transform.position.x;
-        pY = player.transform.position.y;
+        Vector2Int playerlocation = Translate(player.transform.position);
+        Xidx = playerlocation.x;
+        Yidx = playerlocation.y;
+        current_neighbors = findNeighbors(playerlocation);
+        Debug.Log("CN=" + current_neighbors);
     }
 }
         
--- AIProject/Assets/Scripts/ThingsInDungeon/PathNode.cs
+++ AIProject/Assets/Scripts/ThingsInDungeon/PathNode.cs
@@ -2,69 +2,21 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PathNode : MonoBehaviour
+public class PathNode 
 {
+    // !World position to graph position translate function
     // Create static adj matrix 
-    int dfp;
-    public string IDX;
-    public int area; // box around center
-    // X and Y origin of node
-    public double X;
-    public double Y;
-    public bool containsPlayer = false;
-    public List<PathNode> neighbors;
-    private int vIdx; // Index of vertex column of which this node represents
-
-    //Each node tracks it's own scores
-    double fCost;
-    double gScore;
-    double hScore;
-
-
-    public double getX()
+   public Vector2Int nodePosition;
+    public int x;
+    public int y;
+    // create constructor with right click
+    public PathNode(Vector2Int nodePosition)
     {
-        return X;
+        this.nodePosition = nodePosition;
+        x = nodePosition.x;
+        y = nodePosition.y;
     }
 
-    public double getY()
-    {
-        return Y;
-    }
-    // Start is called before the first frame update. new path nodes should specify vertex count
-    void Start()
-    {
-        //initialize
-
-
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-        containsPlayer = hasPlayerWithin();
-        //hScore[vIdx] = distanceCheck(player);
-        if (containsPlayer)
-        {
-            
-        }
-        
-    }
-
-    bool hasPlayerWithin()
-    {
-        GameObject player = GameObject.FindGameObjectWithTag("Player");
-        double pX = player.transform.position.x;
-        double pY = player.transform.position.y;
-        // if player x is greater than node center - 5 AND less than node center -5
-        if ( (pX < X + Mathf.Sqrt(area)/2 ) && (pX > X - (double) Mathf.Sqrt(area)/2 ) ) {
-            //if inside here, then player is within X bounds of this node!
-            if ( (pY < Y + Mathf.Sqrt(area)/2 ) && pY > (Y - Mathf.Sqrt(area)/2 ) )  {
-                //if inside here, then player is also within Y bounds of this node! player is in this node!
-                return true;
-            }
-        }
-            return false;
-        // True if the player is within the bounds of this node.
-    }
 }
+
+
